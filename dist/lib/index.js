e6bfdf1cfbf5dd1263e5f20c5f654dcee7816d5f
zhumin <zhumin@ke.com>
2022-08-24 18:52:02 +0800

    change: command.

--- dist/lib/index.js
+++ dist/lib/index.js
@@ -9,15 +9,29 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const minimist_1 = __importDefault(require("minimist"));
 const fs_1 = __importDefault(require("fs"));
 const path_1 = __importDefault(require("path"));
-// 解析所有【指令】及【参数】
-let argv = (0, minimist_1.default)(process.argv.slice(2));
-// 取出指令
-let commands = argv._;
-// 对应读取 cli 文件夹里的文件（同步）
-let clis = fs_1.default.readdirSync(path_1.default.resolve(__dirname, './cli/')).map(c => c.replace('.js', ''));
-// 找不到指令时默认执行 help
-let cmd = clis.indexOf(commands[0]) !== -1 ? commands[0] : 'help';
-let command = require('./cli/' + cmd).default;
-// 增加运行时的路径信息
-argv.cwd = process.cwd();
-command(argv);
+module.exports = (argv) => {
+    logo();
+    // 解析所有【指令】及【参数】
+    let curArgv = (0, minimist_1.default)(argv.slice(2));
+    // 取出指令
+    let commands = curArgv._;
+    // 对应读取 cli 文件夹里的文件（同步）
+    let clis = fs_1.default.readdirSync(path_1.default.resolve(__dirname, './cli/')).map(c => c.replace('.js', ''));
+    // 找不到指令时默认执行 help
+    let cmd = clis.indexOf(commands[0]) !== -1 ? commands[0] : 'help';
+    let command = require('./cli/' + cmd).default;
+    // 增加运行时的路径信息
+    curArgv.cwd = process.cwd();
+    command(argv);
+};
+function logo() {
+    console.log(`
+                              _                 _     
+        _                    | |               | |    
+    ___| |_  ___   ____ _   _| | _   ___   ___ | |  _ 
+   /___)  _)/ _ \\ / ___) | | | || \\ / _ \\ / _ \\| | / )
+  |___ | |_| |_| | |   | |_| | |_) ) |_| | |_| | |< ( 
+  (___/ \\___)___/|_|    \\__  |____/ \\___/ \\___/|_| \\_)
+                       (____/                                                                  
+    `);
+}
--- lib/index.ts
+++ lib/index.ts
@@ -5,22 +5,36 @@
 import minimist from 'minimist'
 import fs from 'fs'
 import path from 'path'
+import program from 'commander'
 
-// 解析所有【指令】及【参数】
-let argv = minimist(process.argv.slice(2));
+module.exports = (argv: []) => {
+  logo()
+  // 解析所有【指令】及【参数】
+  let curArgv = minimist(argv.slice(2));
 
-// 取出指令
-let commands = argv._
+  // 取出指令
+  let commands = curArgv._
 
-// 对应读取 cli 文件夹里的文件（同步）
-let clis = fs.readdirSync(path.resolve(__dirname, './cli/')).map(c => c.replace('.js', ''))
-// 找不到指令时默认执行 help
-let cmd = clis.indexOf(commands[0]) !== -1 ? commands[0] : 'help'
-let command = require('./cli/' + cmd).default
-// 增加运行时的路径信息
-argv.cwd = process.cwd()
+  // 对应读取 cli 文件夹里的文件（同步）
+  let clis = fs.readdirSync(path.resolve(__dirname, './cli/')).map(c => c.replace('.js', ''))
+  // 找不到指令时默认执行 help
+  let cmd = clis.indexOf(commands[0]) !== -1 ? commands[0] : 'help'
+  let command = require('./cli/' + cmd).default
+  // 增加运行时的路径信息
+  curArgv.cwd = process.cwd()
 
-command(argv)
+  command(argv)
+}
 
-
+function logo() {
+  console.log(`
+                              _                 _     
+        _                    | |               | |    
+    ___| |_  ___   ____ _   _| | _   ___   ___ | |  _ 
+   /___)  _)/ _ \\ / ___) | | | || \\ / _ \\ / _ \\| | / )
+  |___ | |_| |_| | |   | |_| | |_) ) |_| | |_| | |< ( 
+  (___/ \\___)___/|_|    \\__  |____/ \\___/ \\___/|_| \\_)
+                       (____/                                                                  
+    `)
+}
 
